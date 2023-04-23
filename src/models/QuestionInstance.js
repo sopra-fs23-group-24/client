71f3a1758b95666bf55b319ddaeb974a250cfea8
mikej <mikejan.roellin@uzh.ch>
2023-04-23 15:21:04 +0200

    Logic for displaying the correct questions and answers Client Issues 25 26 18

---
+++ src/models/QuestionInstance.js
@@ -0,0 +1,15 @@
+/**
+ * User model
+ */
+class QuestionInstance {
+    constructor(data = {}) {
+        this.questionId = null;
+        this.quizQuestionText = null;
+        this.imageToDisplay = null;
+        this.storyToDisplay = null;
+        this.answerOptions = null;
+        this.correctAnswer = null;
+        Object.assign(this, data);
+    }
+}
+export default QuestionInstance;
