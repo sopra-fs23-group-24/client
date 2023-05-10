8c2a1b84a37c07ee3c74decfcd22f3a364d10c0e
Jan Eggli <jan.eggli@gmx.ch>
2023-05-10 15:43:17 +0200

    Added timer file (client issue #46)

---
+++ src/components/views/timer.js
@@ -0,0 +1,26 @@
+import React, {useEffect, useRef} from 'react';
+
+const CountingTimer = ({timeLeft, setTimeLeft}) => {
+    const timerIntervalRef = useRef(null);
+
+    useEffect(() => {
+        timerIntervalRef.current = setInterval(() => {
+            setTimeLeft(timeLeft => timeLeft - 1);
+        }, 1000);
+        return () => clearInterval(timerIntervalRef.current);
+    }, []);
+
+    useEffect(() => {
+        if (timeLeft === 0) {
+            clearInterval(timerIntervalRef.current);
+        }
+    }, [timeLeft]);
+
+    return (
+        <div>
+            Timer: {timeLeft}
+        </div>
+    );
+};
+
+export default CountingTimer;
