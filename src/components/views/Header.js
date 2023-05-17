0b6e990d58d2f6bcc5ecac223ce2d39bf8771174
mikej <mikejan20@gmail.com>
2023-05-17 13:53:32 +0200

    Client #40 #57

--- src/components/views/Header.js
+++ src/components/views/Header.js
@@ -1,7 +1,23 @@
 import React from "react";
-import {ReactLogo} from "components/ui/ReactLogo";
 import PropTypes from "prop-types";
 import "styles/views/Header.scss";
+import "bootstrap/dist/css/bootstrap.css"; // Import Bootstrap CSS
+import HelpIcon from '@mui/icons-material/Help';
+import {
+    Accordion, AccordionDetails,
+    AccordionSummary,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle, Typography
+} from "@mui/material";
+import {Button} from "../ui/Button";
+
+
+function ExpandMoreIcon() {
+    return null;
+}
 
 /**
  * This is an example of a Functional and stateless component (View) in React. Functional components are not classes and thus don't handle internal state changes.
@@ -11,12 +27,86 @@ import "styles/views/Header.scss";
  * https://reactjs.org/docs/components-and-props.html
  * @FunctionalComponent
  */
-const Header = props => (
-  <div className="header container" style={{height: props.height}}>
-    <h1 className="header title">"Who said that?" - Group 24</h1>
-    <ReactLogo width="60px" height="60px"/>
-  </div>
-);
+
+const Header = props => {
+    const [open, setOpen] = React.useState(false);
+    const handleHelperOpen = () => {
+        console.log("Help clicked")
+        setOpen(true);
+        console.log(open)
+    }
+
+    const handleHelperClose = () => {
+        console.log("Help closed")
+        setOpen(false);
+        console.log(open)
+    }
+
+    const HelperContent = <HelpIcon onClick={handleHelperOpen} sx={{ fontSize: 75 }} color="primary"></HelpIcon>;
+    return (
+        <nav className="navbar navbar-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand mb-0 h1">
+                    <h1>Who said that?</h1>
+                </a>
+                {HelperContent}
+            </div>
+            <Dialog open={open}
+                    onClose={handleHelperClose}
+                    aria-labelledby="alert-dialog-help"
+                    aria-describedby="alert-dialog-text">
+                <DialogTitle id="alert-dialog-title">
+                    {"How does the game work?"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-text">
+                        <Accordion>
+                            <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel1a-content"
+                                id="panel1a-header"
+                            >
+                                <Typography>Accordion 1</Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <Typography>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                                    malesuada lacus ex, sit amet blandit leo lobortis eget.
+                                </Typography>
+                            </AccordionDetails>
+                        </Accordion>
+                        <Accordion>
+                            <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel2a-content"
+                                id="panel2a-header"
+                            >
+                                <Typography>Accordion 2</Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <Typography>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                                    malesuada lacus ex, sit amet blandit leo lobortis eget.
+                                </Typography>
+                            </AccordionDetails>
+                        </Accordion>
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+                        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+                        accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+                        sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+                        consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+                        dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
+                        dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+                        ipsum dolor sit amet.
+                    </DialogContentText>
+                </DialogContent>
+            <DialogActions>
+                <Button onClick={handleHelperClose}>Close</Button>
+            </DialogActions>
+            </Dialog>
+        </nav>
+    );
+}
 
 Header.propTypes = {
   height: PropTypes.string
